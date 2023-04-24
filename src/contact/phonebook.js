d1cfbd558ad6e23f1992764863edd94520a94de4
edem4963 <112492837+edem4963@users.noreply.github.com>
2023-04-24 18:18:20 -0400

    Change http request to backend https request and change http request to app component

--- src/contact/phonebook.js
+++ src/contact/phonebook.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
 const request = axios.get(baseUrl)
@@ -17,7 +17,5 @@ const update = (id, newObject) => {
 }
 
 export default { 
-  getAll: getAll, 
-  create: create, 
-  update: update 
+  getAll, create, update
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import App from './App';
 import axios from 'axios'
 import './index.css'
 
-const promise = axios.get('http://localhost:3001/persons')
+const promise = axios.get('http://localhost:3001/api/persons')
 console.log(promise)
 
 
-axios.get('http://localhost:3001/persons').then(response => {
+axios.get('http://localhost:3001/api/persons').then(response => {
   const persons = response.data
   ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 })
